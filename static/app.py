051d4f9e9e158c7bef62acf777313fa747b71539
sarsteg <85451611+sarsteg@users.noreply.github.com>
2023-07-25 09:29:22 -0500

    flask start

--- static/app.py
+++ static/app.py
@@ -5,11 +5,14 @@ import json
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func 
+from pymongo import MongoClient
 
 #################################################
 # Database Setup
 #################################################
-
+client = MongoClient('mongodb://localhost:27017/')
+db = client['king-bachman']
+collection = db['king-bachman']
 
 #################################################
 # Flask Setup
@@ -20,3 +23,25 @@ app = Flask(__name__)
 # Flask Routes
 #################################################
 
+@app.route('/')
+def home():
+    return (
+        f"Welcome to the King-Bachman API!<br/>"
+        f"Available Routes:<br/>"
+        f"...."
+    )
+
+@app.route('/author')
+def author_review():
+    return render_template('author.html')
+
+@app.route('/book/<book_id>')
+def book_review(book_id):
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    client.close()
+
+if __name__ == '__main__':
+    app.run(debug=True)
